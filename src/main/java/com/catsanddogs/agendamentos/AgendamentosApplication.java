af2c79161b400f3a5b217946afbd8148587fc11e
André Codo <67808063+AcCodo@users.noreply.github.com>
2023-05-17 20:34:31 -0300

    Initial commit

---
+++ src/main/java/com/catsanddogs/agendamentos/AgendamentosApplication.java
@@ -0,0 +1,13 @@
+package com.catsanddogs.agendamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentosApplication.class, args);
+	}
+
+}
