13551cebf0e8578ee444376a46822501644b3d11
André Codo <67808063+AcCodo@users.noreply.github.com>
2023-05-19 20:30:45 -0300

    Editar Especialidade

--- src/main/java/com/catsanddogs/agendamentos/controllers/EspecialidadeController.java
+++ src/main/java/com/catsanddogs/agendamentos/controllers/EspecialidadeController.java
@@ -3,15 +3,19 @@ package com.catsanddogs.agendamentos.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.catsanddogs.agendamentos.models.Especialidade;
 import com.catsanddogs.agendamentos.repositories.EspecialidadeRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/medicos/especialidades")
@@ -36,7 +40,22 @@ public class EspecialidadeController {
 	public String editarGet(Model model, @PathVariable("id") Long id) {
 		Especialidade especialidade = espRep.findById(id).orElse(null);
 		model.addAttribute("especialidade", especialidade);
-		return "/medicos/especialidades/editar";
+		return "/medicos/especialidade/editar";
 	}
+	
+	@PostMapping("/editar/{id}")
+	public String edit(@PathVariable("id") Long id, @Valid @ModelAttribute("viagem") Especialidade espAtualizada, BindingResult result) {
+		if (result.hasErrors()) {
+			return "editar";
+		}
+		
+		Especialidade espExiste = espRep.findById(id).orElse(null);
+		espExiste.setDescricao(espAtualizada.getDescricao());
+		espExiste.setDuracaoConsulta(espAtualizada.getDuracaoConsulta());
+		
+		espRep.save(espExiste);
+		
+		return "redirect:/medicos/especialidades";
+	} 
 	
 }
