964f0fbabd10295ff97f95be22caf8b11fe1b54d
AcCodo <67808063+AcCodo@users.noreply.github.com>
2023-05-24 11:54:27 -0300

    Criar agendamento (sem validação)

--- src/main/java/com/catsanddogs/agendamentos/controllers/AgendamentoController.java
+++ src/main/java/com/catsanddogs/agendamentos/controllers/AgendamentoController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.catsanddogs.agendamentos.models.Agendamento;
+import com.catsanddogs.agendamentos.models.Animal;
+import com.catsanddogs.agendamentos.models.Medico;
 import com.catsanddogs.agendamentos.repositories.AgendamentosRepository;
 import com.catsanddogs.agendamentos.repositories.AnimaisRepository;
 import com.catsanddogs.agendamentos.repositories.MedicosRepository;
@@ -35,29 +38,24 @@ public class AgendamentoController {
 		model.addObject("agendamentos" , ageRep.findAll());
 		return model;
 	}
-	
-	/*
-	
-	@GetMapping("/tutores")
-	public ModelAndView redirectTutores() {
-		ModelAndView model = new ModelAndView("animais/tutores/index");
-		model.addObject("tutores", tutRep.findAll());
-		return model;
-	}
-	
+		
 	@GetMapping("/criar")
 	public ModelAndView criarGet() {
-		ModelAndView model = new ModelAndView("animais/animais/criar");
-		List<Tutor> listT = tutRep.findAll();
-		model.addObject("tutoresList", listT);
+		ModelAndView model = new ModelAndView("agendamentos/criar");
+		List<Animal> animais = aniRep.findAll();
+		List<Medico> medicos = medRep.findAll();
+		model.addObject("animais", animais);
+		model.addObject("medicos", medicos);
 		return model;
 	}
 	
 	@PostMapping("/criar")
-	public String criarPost(@ModelAttribute("animal") Animal novaEsp) {
-		aniRep.save(novaEsp);
-		return "redirect:/animais";
+	public String criarPost(@ModelAttribute("agendamento") Agendamento novoAge) {
+		ageRep.save(novoAge);
+		return "redirect:/agendamentos";
 	}
+
+	/*
 	
 	@GetMapping("/editar/{id}")
 	public ModelAndView editarGet(@PathVariable("id") Long id) {
